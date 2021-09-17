7ad299f63dd54cd8b6e002a89d9269e904c40103
Israel-Beni <bendegbe72@gmail.com>
2021-09-18 03:39:23 +0530

    detailed-custom-validation.html with js

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
-const email = document.getElementById("mail");
+const form = document.getElementById('form')[0];
+console.log(form)
 
-email.addEventListener("input", function (event) {
-    if (email.validity.typeMismatch) {
-        email.setCustomValidity("I am expecting an e-mail address!");
+const email = document.getElementById('mail');
+const emailError = document.querySelector('#mail + span.error');
+
+email.addEventListener('input', function(event) {
+    // Each time a user types something, we check if the form fields are valid.
+    if (email.validity.valid) {
+        // In case there is an error message visible, if the field is valid,
+        // we remove the error message.
+        emailError.textContent = ''; // Reset the content of the message
+        emailError.className = 'error' // Reset the visual state of the message
     } else {
-        email.setCustomValidity("");
+        // If there is still an error, show the correct error
+        showError();
     }
-})
+});
+
+form.addEventListener('submit', function(event) {
+    // if the email field is valid, we let the form submit
+    if (!email.validity.valid) {
+        // If it isn't, we display an appropriate error message
+        showError();
+        // Then we prevent the form from being sent by canceling the event
+        event.preventDefault();
+    }
+});
+
+function showError() {
+    if (email.validity.valueMissing) {
+        // If the field is empty, display the following error message
+        emailError.textContent = 'You need to enter an e-mail address.';
+    } else if (email.validity.typeMismatch) {
+        // If the field doesn't contain an email address, display the following message.
+        emailError.textContent = `Entered value needs to be an e-mail address.`
+    } else if (email.validity.tooShort) {
+        // If the data is too short, display the following error message.
+        emailError.textContent = `Email should be at least ${email.minlength} characters; you entered ${email.value}.`
+    }
+    // Set the styling appropriately
+    emailError.className = 'error active'
+}
+
